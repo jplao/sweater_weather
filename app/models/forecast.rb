ba66143a7cbc5fab337de020940a3d59d3c40eb5
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-03 14:41:37 -0700

    changes initialize method to create forecast objects

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,7 +1,25 @@
 class Forecast
-  attr_reader :location
+  attr_reader :id,
+              :location,
+              :time,
+              :today,
+              :hourly,
+              :five_day
 
-  def initialize(data)
-    @location = data['location']
+  def initialize(data, location)
+    @id = 1
+    @location = location
+    @time = Time.now.strftime('%l:%M%p %m/%Y')
+    @today = CurrentForecast.new(data)
+    @hourly = []
+      data[:hourly][:data].first(8).each do |hour_data|
+        @hourly << HourlyForecast.new(hour_data)
+      end
+    @five_day = []
+    data[:daily][:data][1..5].each do |day_data|
+      @five_day << DailyForecast.new(day_data)
+    end
   end
+
+
 end
