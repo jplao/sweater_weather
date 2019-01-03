13aa7c27b195ebe86cf616e3a20004dffb4fd816
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-02 21:01:46 -0700

    refactors dark sky service

--- app/service/dark_sky_service.rb
+++ app/service/dark_sky_service.rb
@@ -5,8 +5,18 @@ class DarkSkyService
   end
 
   def get_forcast
-    conn = Faraday.new("https://api.darksky.net/forecast/")
-    response = conn.get("#{ENV['DARK_SKY_API_KEY']}/#{@latitude},#{@longitude}")
-    json = JSON.parse(response.body, symbolize_names: true)
+    to_json("#{ENV['DARK_SKY_API_KEY']}/#{latitude},#{longitude}")
+  end
+
+  private
+  attr_reader :latitude,
+              :longitude
+
+  def conn
+    Faraday.new("https://api.darksky.net/forecast/")
+  end
+
+  def to_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
   end
 end
