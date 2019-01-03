9ace9b3e8b4ae659f859dc1076467049d610e2a6
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-02 20:17:41 -0700

    refactors geocode service

--- app/service/geocode_service.rb
+++ app/service/geocode_service.rb
@@ -3,11 +3,22 @@ class GeocodeService
     @location = location
   end
 
-  def get_coords
-    conn = Faraday.new("https://maps.googleapis.com/maps/api/geocode/")
-    response = conn.get("json?address=#{@location}&key=#{ENV['GOOGLE_API_KEY']}")
-    json_response = JSON.parse(response.body, symbolize_names: true)
-    coords = json_response[:results][0][:geometry][:location]
+  def get_data
+    to_json("json?address=#{@location}&key=#{ENV['GOOGLE_API_KEY']}")
   end
 
+  def get_coords
+    get_data[:results][0][:geometry][:location]
+  end
+
+  private
+    attr_reader :location
+
+    def conn
+      Faraday.new("https://maps.googleapis.com/maps/api/geocode/")
+    end
+
+    def to_json(url)
+      JSON.parse(conn.get(url).body, symbolize_names: true)
+    end
 end
