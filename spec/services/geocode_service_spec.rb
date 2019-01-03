0caef961f78270ffa9eeb8bd3b19c31e7891536a
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-02 19:57:23 -0700

    writes test for google geocode service

---
+++ spec/services/geocode_service_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'google geocode service' do
+  before(:each) do
+    @service = GeocodeService.new('denver, co')
+  end
+
+  it 'exists' do
+    expect(@service).to be_an_instance_of(GeocodeService)
+  end
+
+  context 'returns the latitude and longitude of a given location' do
+    it '.get_coords' do
+      expect(@service.get_coords).to eq({"lat": 39.7392358, "lng": -104.990251})
+    end
+  end
+end
