ff50d370ef17d93b983aac98bc7511d51099337e
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-03 15:04:44 -0700

    refactors forecast and facade to move loading data out of initialize

--- app/facade/forecast_facade.rb
+++ app/facade/forecast_facade.rb
@@ -3,15 +3,18 @@ class ForecastFacade
     @location = data['location']
   end
 
+  def create_forecast
+    forecast = Forecast.new(dark_sky, @location)
+    forecast.load_data
+    forecast
+  end
+
+  private
   def geocode
     GeocodeService.new(@location).get_coords
   end
 
   def dark_sky
     DarkSkyService.new(geocode).get_forecast
-  end
-
-  def create_forecast
-    Forecast.new(dark_sky, @location)
   end
 end
--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -7,19 +7,37 @@ class Forecast
               :five_day
 
   def initialize(data, location)
+    @data = data
     @id = 1
     @location = location
     @time = Time.now.strftime('%l:%M%p %m/%Y')
-    @today = CurrentForecast.new(data)
+    @today = []
     @hourly = []
-      data[:hourly][:data].first(8).each do |hour_data|
-        @hourly << HourlyForecast.new(hour_data)
-      end
     @five_day = []
+  end
+
+  def load_data
+    load_today
+    load_hourly
+    load_five_day
+  end
+  
+  private
+    attr_reader :data
+
+  def load_today
+    @today = CurrentForecast.new(data)
+  end
+
+  def load_hourly
+    data[:hourly][:data].first(8).each do |hour_data|
+      @hourly << HourlyForecast.new(hour_data)
+    end
+  end
+
+  def load_five_day
     data[:daily][:data][1..5].each do |day_data|
       @five_day << DailyForecast.new(day_data)
     end
   end
-
-
 end
