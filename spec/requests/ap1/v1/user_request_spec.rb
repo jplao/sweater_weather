9a173f59910b977230143b302ec0d941f4e92c07
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-03 17:35:32 -0700

    changes test to take in params

--- spec/requests/ap1/v1/user_request_spec.rb
+++ spec/requests/ap1/v1/user_request_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 describe 'as a user' do
   it 'creates an account' do
-    data = {
-      "email": "whatever@example.com",
-      "password": "password",
-      "password_confirmation": "password"
+    params = {
+      email: "whatever@example.com",
+      password: "password",
+      password_confirmation: "password"
     }
 
-    post "/api/v1/users"
+    post "/api/v1/users?email=#{params[:email]}&password=#{params[:password]}&password_confirmation=#{params[:password_confirmation]}"
+
+    expect(response).to be_successful
   end
 end
