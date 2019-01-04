53fbce66e1140288737f866b2ecc6f2c68e22e9f
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-04 09:55:38 -0700

    takes in url and source from giphy

--- app/models/day_gif.rb
+++ app/models/day_gif.rb
@@ -1,6 +1,14 @@
 class DayGif
   def initialize(data)
     @summary = data
+    load_gif_data
   end
 
+  def load_gif_data
+    conn = Faraday.new("https://api.giphy.com/v1/gifs/search")
+    response = conn.get("?api_key=#{ENV['GIPHY_API_KEY']}&q=#{@summary}")
+    json_response = JSON.parse(response.body, symbolize_names: true)
+    url = json_response[:data][0][:url]
+    source = json_response[:data][0][:source_tld]
+  end
 end
