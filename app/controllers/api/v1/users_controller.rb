0c3d12ecddefe89d8da7fcc09b0849160d768425
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-03 17:32:05 -0700

    changes method from new to create for post method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::UsersController < ApplicationController
-  def new
+  def create
+    @user = User.create(user_params)
+
+    render json: UserSerializer.new(@user)
   end
+
+  private
+    def user_params
+      params.permit(:email, :password, :password_confirmation)
+    end
 end
