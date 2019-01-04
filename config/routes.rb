32f5c9948f0da0da66aa60018242fa64e5a753c2
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-03 17:33:41 -0700

    changes route from new to create for post request

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resource :forecast, only: :show
-      post '/users', to: 'users#new'
+      post '/users', to: 'users#create'
     end
   end
 end
