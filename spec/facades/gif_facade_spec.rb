2ce0158fd8aa7bdc2bda0b5dfffb0e03fbfd03fd
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-04 11:40:18 -0700

    adds more expectations to gif facade spec

--- spec/facades/gif_facade_spec.rb
+++ spec/facades/gif_facade_spec.rb
@@ -5,5 +5,7 @@ describe 'gif_facade' do
     location = {'location' => 'denver,co'}
     gif_facade = GifFacade.new(location)
     expect(gif_facade).to be_an_instance_of(GifFacade)
+    expect(gif_facade.copywrite).to be_truthy
+    expect(gif_facade.daily_forecasts.count).to eq(8)
   end
 end
