a7cc13b22ce429568a83f81bfd6033e993df3f09
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-05 08:45:12 -0700

    adds sad path session test for wrong password

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,6 +4,8 @@ class Api::V1::SessionsController < ApplicationController
     if @user.authenticate(params[:password])
       session[:user_id] = @user.id
       render json: UserSerializer.new(@user), status: 200
+    else
+      render json: 'Please try again', status: 401
     end
   end
 end
--- spec/requests/ap1/v1/sessions_request_spec.rb
+++ spec/requests/ap1/v1/sessions_request_spec.rb
@@ -1,23 +1,31 @@
 require 'rails_helper'
 
 describe 'as a user' do
-  it 'creates an account with api key' do
-    params = {
+  before(:each) do
+    @params = {
       email: "whatever@example.com",
       password: "password",
     }
 
-    user_1 = User.create(email: params[:email],
-                          password: params[:password],
-                          password_confirmation: params[:password])
+    @user_1 = User.create(email: @params[:email], password: @params[:password])
+  end
 
-    post "/api/v1/sessions?email=#{params[:email]}&password=#{params[:password]}"
+  it 'returns api key and creates session with correct password' do
+
+    post "/api/v1/sessions?email=#{@params[:email]}&password=#{@params[:password]}"
 
     expect(response).to be_successful
     expect(response.status).to eq(200)
 
     request_response = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
     expect(request_response).to have_key(:api_key)
-    expect(request_response[:api_key]).to eq(user_1.api_key)
+    expect(request_response[:api_key]).to eq(@user_1.api_key)
+  end
+
+  it 'does not create session with wrong password' do
+    post "/api/v1/sessions?email=#{@params[:email]}&password=wrong"
+
+    expect(response.status).to eq(401)
+    expect(response.body).to eq("Please try again")
   end
 end
