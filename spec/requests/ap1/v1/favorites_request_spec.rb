bddfe6207c301e98a25266efeb1843245143f6aa
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-05 11:24:33 -0700

    adds conditional to render 401 if bad or no api key

--- spec/requests/ap1/v1/favorites_request_spec.rb
+++ spec/requests/ap1/v1/favorites_request_spec.rb
@@ -3,15 +3,30 @@ require 'rails_helper'
 describe 'as a user' do
   before(:each) do
     @user = create(:user)
+    @location = "Denver, CO"
   end
 
   it 'saves a city to favorites' do
-    location = "Denver, CO"
-    post "/api/v1/favorites?location=#{location}&api_key=#{@user.api_key}"
+    post "/api/v1/favorites?location=#{@location}&api_key=#{@user.api_key}"
 
     expect(response).to be_successful
     expect(response.status).to eq (201)
     favorites = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
-    expect(favorites).to have_key(:cities)
+    expect(favorites).to have_key(:location)
+    expect(@user.favorites.count).to eq(1)
+  end
+
+  it 'does not save favorite with wrong api key' do
+    post "/api/v1/favorites?location=#{@location}&api_key=wrong"
+
+    expect(response.status).to eq(401)
+    expect(response.body).to eq("Please try again")
+  end
+
+  it 'does not save favorite with no api key' do
+    post "/api/v1/favorites?location=#{@location}"
+
+    expect(response.status).to eq(401)
+    expect(response.body).to eq("Please try again")
   end
 end
