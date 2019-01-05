b05bdaa56605e115d90acb95f4b8c0b90ce5d35b
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-05 11:16:14 -0700

    creates favorite serializer and saves location to user favorites

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::FavoritesController < ApplicationController
   def create
+    @user = User.find_by(api_key: params[:api_key])
+    @user.favorites.create(location: params[:location])
+    render json: FavoriteSerializer.new(@user.favorites.last), status: 201
   end
 end
