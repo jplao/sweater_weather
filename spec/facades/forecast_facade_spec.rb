4afdbdfd49e203281af62f6ba8bb5c5f9efbc7cb
Jenn Lao <jenn.p.lao@gmail.com>
2019-01-08 22:15:25 -0700

    adds validation for location and its uniqueness to favorites

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -1,4 +1,3 @@
-
 require "rails_helper"
 
 describe "Forecast Facade" do
--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Favorite, type: :model do
+  describe 'validations' do
+    it {should validate_presence_of(:location)}
+    it {should validate_uniqueness_of(:location)}
+  end
+
   describe 'relationships' do
     it {should belong_to(:user)}
   end
